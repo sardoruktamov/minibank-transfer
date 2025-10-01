a947b9ebe79be91100010fcadea7ec89bb79408e
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2025-10-01 22:13:57 +0500

    update cardDto

--- card/src/main/java/dasturlash/uz/card/dto/CardDTO.java
+++ card/src/main/java/dasturlash/uz/card/dto/CardDTO.java
@@ -1,8 +1,11 @@
 package dasturlash.uz.card.dto;
 
+import dasturlash.uz.card.dto.transaction.TransactionDTO;
 import dasturlash.uz.card.enums.CardStatus;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -13,4 +16,5 @@ public class CardDTO {
     private Long amount = 0l;
     private String ownerId; // profileId
     private CardStatus status;
+    private List<TransactionDTO> transaction;
 }
