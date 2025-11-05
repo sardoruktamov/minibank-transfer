29f814266fd3b4af4759137fa9fce6f8a01cc124
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2025-11-05 20:54:00 +0500

    add retry method

--- gatewayserver/src/main/java/dasturlash/uz/gatewayserver/SpringCloudGatewayRouting.java
+++ gatewayserver/src/main/java/dasturlash/uz/gatewayserver/SpringCloudGatewayRouting.java
@@ -1,11 +1,18 @@
 package dasturlash.uz.gatewayserver;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Configuration
@@ -15,6 +22,14 @@ public class SpringCloudGatewayRouting {
         return builder.routes()
                 .route(p -> p.path("/api/v1/card/**")
                         .filters(f -> f.rewritePath("/api/v1/card/(?<segment>.*)", "/api/v1/card/${segment}")
+                                .retry(config -> config
+                                        .setRetries(3)
+                                        .setStatuses(HttpStatus.INTERNAL_SERVER_ERROR, HttpStatus.BAD_GATEWAY)
+                                        .setBackoff(Duration.ofMillis(100), Duration.ofSeconds(1), 2, true)
+                                )
+//                                .circuitBreaker(config -> config.setName("cardCircuitBreaker")
+//                                        .setFallbackUri("forward:/cardFallbackApi")
+//                                )
 
                         )
                         .uri("lb://CARD")
@@ -24,8 +39,17 @@ public class SpringCloudGatewayRouting {
                                 .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
                                 .circuitBreaker(config -> config.setName("profileCircuitBreaker"))
                         )
+                        .metadata("response-timeout", 3000)
+                        .metadata("connect-timeout", 1000)
                         .uri("lb://PROFILE")
                 ).build();
+    }
+
+    @Bean
+    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build()).build());
     }
 
 }
